4b10aeb558016a1f64fcb5796646e9beddc27ab2
Maric <milosxmaric@gmail.com>
2024-11-24 00:13:02 +0100

    Configured the window of the program and added logo image

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=0)
+
+
+window.mainloop()
